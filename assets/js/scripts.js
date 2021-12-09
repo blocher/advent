b8f30fa4179b7afda8125a4db6fb010a526ad380
Benjamin Locher <blocher@gmail.com>
2021-12-08 21:56:38 -0500

    no js menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,50 @@
-$(document)
-    .ready(function () {
+// $(document)
+//     .ready(function () {
+//
+//         // fix menu when passed
+//         $('.masthead')
+//             .visibility({
+//                 once: false,
+//                 onBottomPassed: function () {
+//                     $('.fixed.menu').transition('fade in');
+//                 },
+//                 onBottomPassedReverse: function () {
+//                     $('.fixed.menu').transition('fade out');
+//                 }
+//             })
+//         ;
+//
+//         // create sidebar and attach to menu open
+//         $('.ui.sidebar')
+//             .sidebar('attach events', '.show-menu')
+//         ;
+//
+//         $('.item').click(function () {
+//             $('.ui.sidebar').sidebar('hide');
+//         });
+//
+//         $(document).on('click', 'a[href^="#"]', function (event) {
+//             event.preventDefault();
+//
+//             $('html, body').animate({
+//                 scrollTop: $($.attr(this, 'href')).offset().top
+//             }, 500);
+//         });
+//
+//     })
+// ;
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+docReady(function() {
 
         // fix menu when passed
         $('.masthead')
@@ -14,22 +59,4 @@ $(document)
             })
         ;
 
-        // create sidebar and attach to menu open
-        $('.ui.sidebar')
-            .sidebar('attach events', '.show-menu')
-        ;
-
-        $('.item').click(function () {
-            $('.ui.sidebar').sidebar('hide');
-        });
-
-        $(document).on('click', 'a[href^="#"]', function (event) {
-            event.preventDefault();
-
-            $('html, body').animate({
-                scrollTop: $($.attr(this, 'href')).offset().top
-            }, 500);
-        });
-
-    })
-;
+});
