0b1bf2cc82c13c3683e0466fc6650f7061448ae0
Benjamin Locher <blocher@gmail.com>
2021-12-11 15:37:45 -0500

    adding metatags, other optimizations

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -34,29 +34,29 @@
 //     })
 // ;
 
-function docReady(fn) {
-    // see if DOM is already available
-    if (document.readyState === "complete" || document.readyState === "interactive") {
-        // call on next available tick
-        setTimeout(fn, 1);
-    } else {
-        document.addEventListener("DOMContentLoaded", fn);
-    }
-}
+// function docReady(fn) {
+//     // see if DOM is already available
+//     if (document.readyState === "complete" || document.readyState === "interactive") {
+//         // call on next available tick
+//         setTimeout(fn, 1);
+//     } else {
+//         document.addEventListener("DOMContentLoaded", fn);
+//     }
+// }
 
-docReady(function() {
+// docReady(function() {
 
-        // fix menu when passed
-        $('.masthead')
-            .visibility({
-                once: false,
-                onBottomPassed: function () {
-                    $('.fixed.menu').transition('fade in');
-                },
-                onBottomPassedReverse: function () {
-                    $('.fixed.menu').transition('fade out');
-                }
-            })
-        ;
+//         // fix menu when passed
+//         $('.masthead')
+//             .visibility({
+//                 once: false,
+//                 onBottomPassed: function () {
+//                     $('.fixed.menu').transition('fade in');
+//                 },
+//                 onBottomPassedReverse: function () {
+//                     $('.fixed.menu').transition('fade out');
+//                 }
+//             })
+//         ;
 
-});
+// });
